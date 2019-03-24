c75a2c08e24a2c9967a24cdeb29083b4682bab0d
Chris Hardin <chris.hardin@oasisdigital.com>
2019-03-23 23:08:06 -0500

    Started modifying ng-bootstrap carousel source

---
+++ projects/ngbootstrap-carousel-extended/src/public_api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ngbootstrap-carousel-extended
+ */
+
+export * from './lib/ngbootstrap-carousel-extended.service';
+export * from './lib/ngbootstrap-carousel-extended.component';
+export * from './lib/ngbootstrap-carousel-extended.module';
